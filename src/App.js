683a49b840c1486b8b9e1ccaaafc5c17637d4ee6
Tomasz Jader <tomasz.jader@gamil.com>
2021-10-22 19:01:43 +0200

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import ReactDOM from "react-dom";
+
+
+const SimpleNavigation = (props) => {
+  const fetchMoreData = (i) => {
+    const req = new XMLHttpRequest();
+    req.open(
+      "GET",
+      `https://rickandmortyapi.com/api/character/?page=${i}`,
+      true
+    );
+    req.send();
+    req.onload = () => {
+      if (req.status === 200) {
+        const myresponseText = JSON.parse(req.responseText);
+        const results = myresponseText.results;
+
+        props.setData(results);
+      }
+    };
+  };
+  return (
+    <nav className="menu">
+      <ul className="nav-bar">
+        {props.curentPages.map((i) => {
+          
+          return (
+            <li key={i} className="list-nav">
+              <a href="#top" onClick={() => fetchMoreData(i)} > {i}  </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 const Character = (props) => {
   return (
-    <li key={props.id} className="list" >
+    <li key={props.id} className="list">
       <ul className="list--style">
         <li className="list--header">{props.d.name}</li>
         <li className={props.d.status === "Alive" ? "alive" : "dead"}>
@@ -13,7 +48,9 @@ const Character = (props) => {
         <li>{props.d.species}</li>
         <li>{props.d.type ? props.d.type : "no type"}</li>
         <li>{props.d.gender}</li>
-        <li>{props.d.created}</li>
+        <li>
+          {props.d.created.slice(0, 10)} {props.d.created.slice(11, 16)}
+        </li>
       </ul>
       <img
         className="image"
@@ -23,14 +60,14 @@ const Character = (props) => {
       />
     </li>
   );
-}
+};
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
+
   const [search, setSearch] = useState("");
-  const [isScroling, setIsScroling] = useState(true);
-  
+
+  const curentPages = Array.from({ length: 34 }, (_, i) => i + 1);
 
   useEffect(() => {
     const req = new XMLHttpRequest();
@@ -43,38 +80,13 @@ const App = () => {
         console.log(myresponseText);
         const results = myresponseText.results;
         setData(results);
-        
       }
     };
   }, []);
-  const fetchMoreData = () => {
-    if (page === 34) {
-      setIsScroling(false);
-      return;
-    }
-    const req = new XMLHttpRequest();
-    req.open(
-      "GET",
-      `https://rickandmortyapi.com/api/character/?page=${page}`,
-      true
-    );
-    req.send();
-    req.onload = () => {
-      if (req.status === 200) {
-        const myresponseText = JSON.parse(req.responseText);
-        console.log(myresponseText);
-        const results = myresponseText.results;
-        const tmpData = data;
-        setData(tmpData.concat(results));
-        setPage(page + 1);
-      }
-    };
-  };
-  
-  
+
   return (
-    <div>
-      <div className="serch">
+    <main >
+      <div className="serch" id='top'>
         <input
           className="serch-input"
           type="text"
@@ -84,32 +96,24 @@ const App = () => {
           }}
         />
       </div>
-
-      <ul>
-        <InfiniteScroll
-          className="conteiner"
-          dataLength={data.length}
-          next={() => fetchMoreData()}
-          hasMore={isScroling}
-          loader={<h4>Loading...</h4>}
-        >
-          {data
-            .filter((d) => {
-              if (search === "") {
-                return d;
-              } else if (d.name.toLowerCase().includes(search.toLowerCase())) {
-                return d;
-              }
-            })
-            .map((d, id) => {
-              if (id === d.length - 1) {
-                return <Character key={id} id={id} d={d} />;
-              }
-              return <Character key={id} id={id} d={d} />;
-            })}
-        </InfiniteScroll>
+      <ul className="conteiner">
+        {data
+          .filter((d) => {
+            if (search === "") {
+              return d;
+            } else if (d.name.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            }
+          })
+          .map((d, id) => {
+            return <Character key={id} id={id} d={d} />;
+          })}
       </ul>
-    </div>
+      <SimpleNavigation
+        curentPages={curentPages}
+        setData={setData}
+      ></SimpleNavigation>
+    </main>
   );
 };
 
